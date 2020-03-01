14bcb5675badfb872a5ed768c6665c4be43760b5
Carleigh Crockett <capleugh@gmail.com>
2020-03-01 04:27:42 -0700

    Add attributes to forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,7 +1,6 @@
 class ForecastFacade
   def initialize(location_data)
-    # @location = location_data[:location]
-    # not sure what to put in here. doesn't even hit my pry
+    @lattitude = location_data[:lat]
+    @longitude = location_data[:lng]
   end
-
 end
