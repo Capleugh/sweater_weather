dd04a69c24a09163dcc9ccb878c02fa7708de497
Carleigh Crockett <capleugh@gmail.com>
2020-03-02 11:17:50 -0700

    Update dark sky api call

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -3,14 +3,10 @@ class DarkSkyService
     weather_data = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{location.lat},#{location.lng}")
 
     JSON.parse(weather_data.body, symbolize_names: true)
-    # require "pry"; binding.pry
   end
 
   def get_future_forecast_json(destination)
-    # require "pry"; binding.pry
-    time = destination["routes"][0]["legs"][0]["duration"]["value"] + Time.now.to_i
-
-    forecast_data = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{destination.lat},#{destination.long},#{time}")
+    forecast_data = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{destination.lat},#{destination.lng},#{destination.time}")
 
     json = JSON.parse(forecast_data.body, symbolize_names: true)
     # require "pry"; binding.pry
