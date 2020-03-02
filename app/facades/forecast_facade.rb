853a3678580560aaf3a7ee50da5583a6f6499e7d
Carleigh Crockett <capleugh@gmail.com>
2020-03-02 07:18:31 -0700

    Add get_forecast to forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,6 +1,23 @@
 class ForecastFacade
-  def initialize(location_data)
-    @lattitude = location_data[:lat]
-    @longitude = location_data[:lng]
+  def initialize(location)
+    @location = location
+  end
+
+  def get_location
+    Location.new(get_location_data)
+  end
+
+  def get_forecast
+    Forecast.new(get_weather_data, get_location)
+  end
+
+  def get_location_data
+    service = GeocodeService.new
+    service.get_location_json(@location)
+  end
+
+  def get_weather_data
+    service = DarkSkyService.new
+    service.get_forecast_json(get_location)
   end
 end
