d2eeb5c4bbcbcac7808d3f4a0f99a6f48d680a9c
Carleigh Crockett <capleugh@gmail.com>
2020-03-02 07:12:57 -0700

    Create google geocode api call test

--- spec/services/google_geocode_service_spec.rb
+++ spec/services/google_geocode_service_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe GeocodeService, :vcr do
-  it "and returns that city's latitudinal and longitudinal coordinates" do
+  it "returns a city's latitudinal and longitudinal coordinates" do
     geo_service = GeocodeService.new
-    json = geo_service.get_coordinate_json('Denver, CO')
+    json = geo_service.get_location_json('Denver, CO')
 
     expect(json[:results][0][:geometry][:location][:lat]).to eq(39.7392358)
     expect(json[:results][0][:geometry][:location][:lng]).to eq(-104.990251)
