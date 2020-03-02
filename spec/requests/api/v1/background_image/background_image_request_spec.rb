c688d513ce2811f22b911d2fae572a2c0bba90ff
Carleigh Crockett <capleugh@gmail.com>
2020-03-02 09:28:48 -0700

    Setup for background image

---
+++ spec/requests/api/v1/background_image/background_image_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "background image api", :vcr do
+  it "sends information about an image for a specific location" do
+    get "/api/v1/backgrounds?location=denver,co"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+end
