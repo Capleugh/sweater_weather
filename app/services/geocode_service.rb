53ffebf03aa56b58e66b720813869494dce758b8
Carleigh Crockett <capleugh@gmail.com>
2020-03-02 07:15:21 -0700

    Create geocode service

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,5 +1,5 @@
 class GeocodeService
-  def get_coordinate_json(location)
+  def get_location_json(location)
     coordinate_data = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
       req.params['address'] = location
       req.params['key'] = ENV['GOOGLE_API_KEY']
