97955727b2d8155bd8c98842a90064f6d51634fc
Carleigh Crockett <capleugh@gmail.com>
2020-03-02 23:40:57 -0700

    Remove commented out pry

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -3,6 +3,5 @@ class DarkSkyService
     weather_data = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{location.lat},#{location.lng}")
 
     json = JSON.parse(weather_data.body, symbolize_names: true)
-    # require "pry"; binding.pry
   end
 end
