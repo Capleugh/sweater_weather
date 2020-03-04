32d1dadde690ee6953a617079f8d563bf17bfd00
Carleigh Crockett <capleugh@gmail.com>
2020-03-03 22:55:56 -0700

    Create conditional to handle what happens if a user does not save

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    
+    user = User.create!(user_params)
+    if user.save
+      response.status = 200
+      # render json in here
+    else
+      response.status = 401
+      # require "pry"; binding.pry
+      # render some json in here
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
