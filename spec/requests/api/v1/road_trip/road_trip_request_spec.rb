2b74471cd2085816bb05623f621465f5f3a77a12
Carleigh Crockett <capleugh@gmail.com>
2020-03-04 06:47:34 -0700

    Stub out current time

--- spec/requests/api/v1/road_trip/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip/road_trip_request_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'road trip api', :vcr do
   it 'sends information about' do
+    allow(Time).to receive(:now).and_return(Time.parse('2020-03-02 11:22:54 -0700'))
+
     user = User.create(email: "whatever@example.com", password: "password")
 
     road_trip_params = {
-      origin: "Denver,CO",
+      origin: "Denver, CO",
       destination: "Pueblo, CO",
       api_key: user.api_key
     }
