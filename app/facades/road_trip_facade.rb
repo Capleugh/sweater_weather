153906ca101685d8dc526e85f91b090b5570a916
Carleigh Crockett <capleugh@gmail.com>
2020-03-04 07:25:55 -0700

    Create poro to handle passing an object to serializer

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -8,7 +8,11 @@ class RoadTripFacade
     @api_key = road_trip_data[:api_key]
   end
 
-  def get_travel_time
+  def get_road_trip
+    RoadTrip.new(@origin, @destination, get_destination_info, get_forecast)
+  end
+
+  def get_destination_info
     Destination.new(get_directions_data)
   end
 
@@ -23,6 +27,6 @@ class RoadTripFacade
 
   def get_future_forecast_data
     service = DarkSkyService.new
-    service.get_future_forecast_json(get_destination)
+    service.get_future_forecast_json(get_destination_info)
   end
 end
