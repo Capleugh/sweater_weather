b7505fad078a6285b7a5e7abb922b0b96e152128
Carleigh Crockett <capleugh@gmail.com>
2020-03-04 07:56:14 -0700

    Create assertion which tests that road trip info cannot be accessed without apikey or user

--- spec/requests/api/v1/road_trip/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip/road_trip_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'road trip api', :vcr do
-  it 'sends information about' do
+  it "sends information about a user's road trip" do
     allow(Time).to receive(:now).and_return(Time.parse('2020-03-02 11:22:54 -0700'))
 
     user = User.create(email: "whatever@example.com", password: "password")
@@ -14,7 +14,11 @@ RSpec.describe 'road trip api', :vcr do
 
     post '/api/v1/road_trip', params: road_trip_params
 
+    expect(response).to be_successful
+
     road_trip_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(:ok)
 
     attributes = road_trip_json[:data][:attributes]
 
@@ -22,5 +26,19 @@ RSpec.describe 'road trip api', :vcr do
     expect(attributes).to have_key(:destination)
     expect(attributes).to have_key(:travel_time)
     expect(attributes).to have_key(:forecast)
+  end
+
+  it "pemission denied if not a user" do
+    allow(Time).to receive(:now).and_return(Time.parse('2020-03-02 11:22:54 -0700'))
+
+    road_trip_params = {
+      origin: "Denver, CO",
+      destination: "Pueblo, CO"
+    }
+
+    post '/api/v1/road_trip', params: road_trip_params
+
+    expect(response).to_not be_successful
+    expect(response).to have_http_status(:unauthorized)
   end
 end
