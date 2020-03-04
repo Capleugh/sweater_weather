69f25c273adb24d1b1633b6d1befc6b027320093
Carleigh Crockett <capleugh@gmail.com>
2020-03-04 02:01:07 -0700

    Create api_key column on users table and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_022651) do
+ActiveRecord::Schema.define(version: 2020_03_04_085813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_03_04_022651) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "api_key"
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
 end
--- spec/factories/users.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    email { "MyString" }
-    password_digest { "MyString" }
-  end
-end
