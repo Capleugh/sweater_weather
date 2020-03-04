8354d194c87b4917f7bec98352def4e53a7ef602
Carleigh Crockett <capleugh@gmail.com>
2020-03-04 06:49:48 -0700

    Add helper methods to road trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,10 +1,28 @@
 class RoadTripFacade
   attr_reader :id
 
-  def initialize(origin, destination, api_key)
+  def initialize(road_trip_data)
     @id = nil
-    @origin = origin
-    @destination = destination
-    @api_key = api_key
+    @origin = road_trip_data[:origin]
+    @destination = road_trip_data[:destination]
+    @api_key = road_trip_data[:api_key]
+  end
+
+  def get_travel_time
+    Destination.new(get_directions_data)
+  end
+
+  def get_forecast
+    DestinationForecast.new(get_future_forecast_data)
+  end
+
+  def get_directions_data
+    service = GeocodeService.new
+    service.get_directions_json(@origin, @destination)
+  end
+
+  def get_future_forecast_data
+    service = DarkSkyService.new
+    service.get_future_forecast_json(get_destination)
   end
 end
