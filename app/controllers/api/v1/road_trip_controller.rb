e138aa10e7f790751807072e81d7911b5f952dab
Carleigh Crockett <capleugh@gmail.com>
2020-08-17 08:55:28 -0600

    Add user to road_trip_controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     road_trip = RoadTripFacade.new(road_trip_params)
-    
-    if road_trip.api_key
+    user = User.find_by(api_key: params[:api_key])
+
+    if user && road_trip.api_key
       response.status = 200
 
       render json: RoadtripSerializer.new(road_trip.get_road_trip)
--- spec/requests/api/v1/road_trip/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip/road_trip_request_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe 'road trip api', :vcr do
 
     user = User.create(email: "whatever@example.com", password: "password")
 
+    user_params = {
+      email: "whatever@example.com",
+      password: "password"
+    }
+
+    post '/api/v1/sessions', params: user_params
+    expect(response).to be_successful
+
+
     road_trip_params = {
       origin: "Denver, CO",
       destination: "Pueblo, CO",
@@ -34,6 +43,35 @@ RSpec.describe 'road trip api', :vcr do
     road_trip_params = {
       origin: "Denver, CO",
       destination: "Pueblo, CO"
+    }
+
+    post '/api/v1/road_trip', params: road_trip_params
+
+    expect(response).to_not be_successful
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  it "pemission denied if wrong api key" do
+    allow(Time).to receive(:now).and_return(Time.parse('2020-03-02 11:22:54 -0700'))
+
+    user_1 = User.create(email: "whatever@example.com", password: "password", api_key: "someNons3nse")
+
+    user_params = {
+      email: "whatever@example.com",
+      password: "password"
+    }
+
+    post '/api/v1/sessions', params: user_params
+
+    expect(response).to be_successful
+    user_json = JSON.parse(response.body)
+
+    expect(user_json["data"]["attributes"]["api_key"]).to eq(user_1.api_key)
+
+    road_trip_params = {
+      origin: "Denver, CO",
+      destination: "Pueblo, CO",
+      api_key: !user_1.api_key
     }
 
     post '/api/v1/road_trip', params: road_trip_params
